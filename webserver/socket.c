11b9b0d710479c4037720d5554beba35de648b08
Robin LEFEBVRE <robin1.lefebvre@etudiant.univ-lille1.fr>
2016-09-20 14:19:13 +0200

    bis

--- webserver/socket.c
+++ webserver/socket.c
@@ -2,19 +2,33 @@
 #include <string.h>
 
 
-int socket () {
 
-int socket_serveur;
+  
+  int socket_serveur;
+  
+  socket_serveur = socket(AF_INET, STOCK_STREAM,0);
+  
+  if (socket_serveur == -1) {
+    
+    perror("socket_serveur");
+    
+  }
 
-socket_serveur = socket(AF_INET, STOCK_STREAM,0);
+struct sockaddr_in {
+  sa_family_t sin_family;
+  in-port_t sin_port;
+  struct in-addr sin_addr;
 
- if (socket_serveur == -1) {
-
-   perror("socket_serveur");
-   
- }
+}
 
 
- 
+  if (listen(socket_serveur,10)==-1) {
+
+    perror("Listen socket_serveur");
 
   }
+
+
+
+  
+ 
---
+++ webserver/socket.c~
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int socket () {
+
+int socket_serveur;
+
+socket_serveur = socket(AF_INET, STOCK_STREAM,0);
+
+ if (socket_serveur == -1) {
+
+   perror("socket_serveur");
+   
+ }
+
+
+ 
+
+  }
