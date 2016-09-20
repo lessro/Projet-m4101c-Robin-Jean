786ef90cabc3df71df276ea8dcd7d54dd8110ea2
Simon DEFRETIN <defretis@hevea03.iut-infobio.priv.univ-lille1.fr>
2016-09-20 14:30:17 +0200

    toto

--- webserver/socket.c
+++ webserver/socket.c
@@ -2,33 +2,4 @@
 #include <string.h>
 
 
-
-  
-  int socket_serveur;
-  
-  socket_serveur = socket(AF_INET, STOCK_STREAM,0);
-  
-  if (socket_serveur == -1) {
-    
-    perror("socket_serveur");
-    
-  }
-
-struct sockaddr_in {
-  sa_family_t sin_family;
-  in-port_t sin_port;
-  struct in-addr sin_addr;
-
-}
-
-
-  if (listen(socket_serveur,10)==-1) {
-
-    perror("Listen socket_serveur");
-
-  }
-
-
-
-  
  
