31210393618a46310827034a86808c845133fd6b
Jean HEYSEN <jean.heysen@etudiant.univ-lille1.fr>
2016-09-27 15:03:01 +0200

    init signal

--- webserver/main.c
+++ webserver/main.c
@@ -9,8 +9,11 @@
 #include <stdlib.h>
 #include "socket.h"
 
+void initialiser_signaux(void);
+
 int main(void)
 {
+	initialiser_signaux();
 	//if(argc > 1 && argv[1] == ""){
 	creer_serveur(8888);
 	//}else{
@@ -21,4 +24,12 @@ int main(void)
 
 	return 0;
 	
+}
+
+void initialiser_signaux(void){
+	if ( signal (SIGPIPE,SIG_IGN) == SIG_ERR ) {
+		perror ( " signal ");
+	}
+
+
 }
