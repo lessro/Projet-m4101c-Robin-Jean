378c4610bbf6b6f7cc1975eb456f34016c808ed6
Simon DEFRETIN <defretis@bouleau04.iut-infobio.priv.univ-lille1.fr>
2016-10-26 17:25:50 +0200

    Chapitre 6 entamé

--- webserver/socket.c
+++ webserver/socket.c
@@ -9,6 +9,83 @@
 #include <stdlib.h>
 #include "socket.h"
 
+
+
+const char * erreur400 = "HTTP/1.1 400 Bad request\r\nConnection: close\r\nContent-length: 17\r\n\r\n400 Bad request\r\n";
+const char * erreur404 = "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-length: 15\r\n\r\n404 Not Found\r\n";
+
+enum http_method {
+  HTTP_GET,
+  HTTP_UNSUPPORTED ,
+};
+
+typedef struct{
+  enum http_method method;
+  int major_version;
+  int minor_version;
+  char *target;
+} http_request;
+
+
+int parse_http_request(const char * request_line, http_request *request){
+    
+
+    char *tmpLine = (char *) malloc(sizeof(request_line));
+
+    strcpy(tmpLine,request_line);
+
+    if(strcmp(strtok(tmpLine," "),"GET") != 0){
+      request->method=HTTP_UNSUPPORTED;
+      return 0;
+    }
+
+    request->target = strtok(NULL," ");
+ 
+
+    char * mot3 = strtok(NULL," ");//on met de cote le 3e mot pr lanalyser apres
+    
+    //on verifie quil ny a pas de 4e mot
+    
+    if(strcmp(mot3,"HTTP/1.0\n") == 0 || strcmp(mot3,"HTTP/1.0\r\n") == 0){
+      request->major_version=1;
+      request->minor_version=0;
+      request->method = HTTP_GET;
+    }
+    else if(strcmp(mot3,"HTTP/1.1\n") == 0 || strcmp(mot3,"HTTP/1.1\r\n") == 0){
+      request->major_version=1;
+      request->minor_version=1;
+      request->method = HTTP_GET;
+
+    }else{
+      return 0;
+    }
+   
+
+    if(strtok(NULL," ") != NULL){
+      return 0;   
+    }
+    return 1;
+}
+
+
+void fgets_or_exit(char *buffer, int size, FILE * stream){
+    char *rep = fgets(buffer,size,stream);
+    if(rep == NULL){
+      exit(0);
+    }
+}
+
+void skip_headers(FILE *client){
+  char tmpBuffer[200];
+  fgets(tmpBuffer,200,client);
+
+  while(strcmp(tmpBuffer,"\n") != 0 && strcmp(tmpBuffer,"\r\n") != 0){
+    fgets(tmpBuffer,200,client);
+  }
+}
+
+
+
 int creer_serveur(int port){
   //const char *msg_bienvenue = "  /\\_/\\ \n=( °w° )= \n  )   (  // \n (__ __)// \n ";
   int sock = socket(AF_INET,SOCK_STREAM,0);
@@ -49,85 +126,56 @@ int creer_serveur(int port){
     if(client == -1){
       perror("erreur accept");
     }else{
+
+
+
       pid=fork();
       switch(pid){
       case -1:
-	perror("bug de fork!!");
-	exit(1);
-	break;
+      	perror("bug de fork!!");
+      	exit(1);
+      	break;
 
       case 0:
-	{
-	  FILE  * fichier = fdopen(client,"w+");
+  	 {
+        int isErreur400=0;
+        int isErreur404=0;
+    	  FILE  * fichier = fdopen(client,"w+");
 
-	  char buffer[200];
+        int taille = 200;
 
-	  fgets(buffer,200,fichier);
+    	  char buffer[taille];
 
-	  printf("recu : %s",buffer);
+        fgets_or_exit(buffer,taille,fichier);
 
-    int isErreur400 = 0;
-    int isErreur404 =0;
+        http_request * req = malloc(sizeof(http_request));
 
-	  const char * erreur400 = "HTTP/1.1 400 Bad request\r\nConnection: close\r\nContent-length: 17\r\n\r\n400 Bad request\r\n";
-	  const char * erreur404 = "HTTP/1.1 404 Not Found\r\nConnection: close\r\nContent-length: 15\r\n\r\n404 Not Found\r\n";
-    
-	  //VERIFICATIONS REQUETE
-	  //verification premier mot = GET
-	  if(strcmp(strtok(buffer," "),"GET") != 0){
-	    isErreur400=1;
-
-	  }
-
-	  //verification deuxieme mot existe
-	  else if(strcmp(strtok(NULL," "),"/")!=0){
-	    isErreur404=1;
-
-	  }
-	  char * mot3 = strtok(NULL," ");//on met de cote le 3e mot pr lanalyser apres
-	  
-	  //on verifie quil ny a pas de 4e mot
-	  printf("%s", mot3);
-
-	  if(strncmp(mot3,"HTTP/1.0",8) != 0 && strncmp(mot3,"HTTP/1.1",8) != 0 ){
-	    isErreur400 =1;
-      printf("erreur 3éme mot\n");
-	  }
-
-    else if(strtok(NULL," ") != NULL){
-      isErreur400=1;  
-      printf("mot 4 pas sencé être al\n");     
-    }
+        if(parse_http_request(buffer,req)==0){
+          isErreur400=1;
+        }
+        skip_headers(fichier);
 
 
-	  while(strcmp(buffer,"\n") != 0 && strcmp(buffer,"\r\n") != 0){
-	    fgets(buffer,200,fichier);
-	    
-	  }
+            
+        if(isErreur400==1){
+          fprintf(fichier, "%s\n", erreur400);
+          
+        }
+        else if(isErreur404==1){
+          fprintf(fichier, "%s\n", erreur404);
+          
+        }
 
-        
-    if(isErreur400==1){
-      fprintf(fichier, "%s\n", erreur400);
-      
-    }
-    else if(isErreur404==1){
-      fprintf(fichier, "%s\n", erreur404);
-      
-    }
+        else if (fprintf (fichier,"pawnee %s",buffer)==-1){
+          perror ("erreur de transposition");
+        }
+            
+        fflush(fichier);
 
-	  else if (fprintf (fichier,"pawnee %s",buffer)==-1){
-	    perror ("erreur de transposition");
-	  }
-        
-        
-
-	  fflush(fichier);
-
-  
-	  close(client);
-	  exit(0);
-	  break;
-	}
+    	  close(client);
+    	  exit(0);
+    	  break;
+    	}
       default:
 	close(client);
 	//retour début de la boucle, attente nouvelle connection (process principal)
@@ -138,3 +186,12 @@ int creer_serveur(int port){
 
   return sock;
 }
+
+
+
+
+
+
+
+
+ 
